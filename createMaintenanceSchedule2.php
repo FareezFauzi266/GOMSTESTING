4221472c881824bacdc50c3fe65f2522c4867346
aunifthh <auniauzi@gmail.com>
2025-06-26 12:50:52 +0800

    Update createMaintenanceSchedule2.php

--- createMaintenanceSchedule2.php
+++ createMaintenanceSchedule2.php
@@ -170,8 +170,7 @@
                 <option value="">Select Frequency</option>
                 <option value="1">Daily</option>
                 <option value="7">Weekly</option>
-                <option value="14">Bi-weekly</option>
-              </select>
+                <option value="14">Bi-weekly</option></select>
             </div>
           </div>
           <div class="modal-footer">
