9032f86efe7b36dc908fb5513913f4238c80af7c
nadhirah <ainnurnadhirah04@gmail.com>
2025-07-20 13:54:14 +0800

    minor adjustment at logout

--- navbar/managernavbar.php
+++ navbar/managernavbar.php
@@ -17,7 +17,7 @@ if (isset($_GET['logout'])) {
     }
 
     // Redirect to index.php (login)
-    header("Location: /GOMS/index.php");
+    header("Location: /gomstesting/GOMSTESTING/index.php");
     exit;
 }
 ?>
